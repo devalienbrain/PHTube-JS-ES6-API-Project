a3af1966084190aaf4e81cf2a6e2c97e543f4079
Alien Brain <murad.dub@gmail.com>
2023-08-31 23:23:00 +0600

    blog html added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Category Api Loading codes
 const loadCategoryApiData = async() =>{
 const apiCategoryData = await fetch('https://openapi.programming-hero.com/api/videos/categories');
 const categoryData = await apiCategoryData.json();
@@ -8,12 +9,21 @@ loadCategoryApiData();
 function displayCategoryApiData(categoryData){
   const categoryContainer = document.getElementById('category-container');
   const categories = categoryData.data;
-   console.log(categories);
    categories.forEach(category => {
     const a = document.createElement('a');
-    a.innerHTML=`<button class="flex justify-center items-center bg-gray-200 hover:bg-red-600 hover:text-white py-2 px-5 rounded-sm">${category.category}</button>
+    a.innerHTML=`<button class="flex justify-center items-center bg-gray-200 hover:bg-red-500 hover:text-white py-2 px-5 rounded-sm">${category.category}</button>
     `;
 categoryContainer.appendChild(a);
    });
 }
 
+// Api all data loading codes
+const categoryWiseLoadApiData = async() =>{
+  const apiDataByCategory = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+  const dataByCategory = await apiDataByCategory.json();
+  showApiDataByCategory(dataByCategory);
+}
+categoryWiseLoadApiData();
+function showApiDataByCategory(dataByCategory){
+  console.log(dataByCategory.data);
+}
