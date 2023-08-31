808538f2f80e84d19ea75c1e4d49e9ea02e562fc
Alien Brain <murad.dub@gmail.com>
2023-08-31 20:36:24 +0600

    dynamic category api done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const loadCategoryApiData = async() =>{
+const apiCategoryData = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+const categoryData = await apiCategoryData.json();
+displayCategoryApiData(categoryData);
+}
+loadCategoryApiData();
+
+function displayCategoryApiData(categoryData){
+  const categoryContainer = document.getElementById('category-container');
+  const categories = categoryData.data;
+   console.log(categories);
+   categories.forEach(category => {
+    const a = document.createElement('a');
+    a.innerHTML=`<button class="flex justify-center items-center bg-gray-200 hover:bg-red-600 hover:text-white py-2 px-5 rounded-sm">${category.category}</button>
+    `;
+categoryContainer.appendChild(a);
+   });
+}
+
